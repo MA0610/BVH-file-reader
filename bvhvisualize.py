3e3a717146fe8f8ad369f1b465ad98150d9ef4ff
MA0610 <47308923+MA0610@users.noreply.github.com>
2025-04-11 12:31:02 -0700

    changed update function to make it so plotted points are centered (immediately in frame once plot opens)

--- bvhvisualize.py
+++ bvhvisualize.py
@@ -63,8 +63,20 @@ class BVHAnimator:
         return self.scat,
 
     def update(self, i):
-        idx = min(i, self.bvh.numFrames()-1)
-        x,y,z = self.read_frame(idx)
-        self.title.set_text("Frame %d"%idx)
-        self.scat._offsets3d = (x,y,z) 
+        idx = min(i, self.bvh.numFrames() - 1)
+        
+        # Read the frame data for the given index
+        x, y, z = self.read_frame(idx)
+        
+        # Compute the center (mean) of the joint positions for this frame
+        joint_positions = np.array([x, y, z]).T  # Stack the x, y, z into a single array (Nx3)
+        center = np.mean(joint_positions, axis=0)
+        
+        # Center the joint positions by subtracting the center
+        centered_positions = joint_positions - center
+
+        # Update the plot with centered positions
+        self.title.set_text("Frame %d" % idx)
+        self.scat._offsets3d = (centered_positions[:, 0], centered_positions[:, 1], centered_positions[:, 2])
+
         return self.scat,
